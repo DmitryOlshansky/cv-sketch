06d8bd0cd2902a0f2e663e7af237f004e72b43d0
Dmitry Olshansky <dmitry.olsh@gmail.com>
2016-05-30 17:04:28 +0300

    Refine Line RHT with epouchs and iterative points removal

--- hough.py
+++ hough.py
@@ -9,14 +9,14 @@ img = cv2.blur(img, (3,3))
 img = cv2.Canny(img, 120, 200)
 
 def draw_fit(x, y):
-	m,b = fit_line(img, x, y)
+	m,b = ls_fit_line(img, x, y)
 	cv2.line(original, (0, int(b)), (500, int(m*500+b)), (255,0,0))	
 
 def draw_line(m, b):
 	cv2.line(original, (0, int(b)), (500, int(m*500+b)), (255,0,0))	
 
-
-def fit_line(img, x, y):
+# least-squares fit line
+def ls_fit_line(img, x, y):
 	height, width = img.shape[0:2]
 	sx = -3+x if x >= 3 else 0
 	sy = -3+y if y >= 3 else 0
@@ -63,9 +63,9 @@ def rht_ellipse_step(img, pts):
 	x2,y2 = pts[1][0], pts[1][1]
 	x3,y3 = pts[2][0], pts[2][1]
 	try:
-		m1, b1 = fit_line(img, x1, y1)
-		m2, b2 = fit_line(img, x2, y2)
-		m3, b3 = fit_line(img, x3, y3)
+		m1, b1 = ls_fit_line(img, x1, y1)
+		m2, b2 = ls_fit_line(img, x2, y2)
+		m3, b3 = ls_fit_line(img, x3, y3)
 
 		t12 = intersection((m1, b1), (m2, b2))
 		t23 = intersection((m2, b2), (m3, b3))
@@ -130,45 +130,22 @@ def rht_ellipses(img, steps, tolerance=4):
 		cv2.ellipse(original, (int(x),int(y)), (int(r1), int(r2)), 0, 0, 360, (0, 0, 255))
 		print v
 
-def rht_lines_step(sample):
-	x1,y1 = sample[0]
-	x2,y2 = sample[1]
-	if x1 != x2: 
-		m = (y1 - y2)/(x1 - x2)
-		b = y1 - x1 * m
-	else:
-		m = float('inf')
-		b = x1
-	return m,b
-
-def rht_lines(img, steps, tolerance=4):
-	accum = LinesAccum(tolerance)
-	points = []
-	it = np.nditer(img, flags=["multi_index"])
-	while not it.finished:
-		if it[0] > 0:
-			points.append((it.multi_index[1], it.multi_index[0]))
-		it.iternext()
-	for i in xrange(steps):
-		sample = random.sample(points, 2)
-		e = rht_lines_step(sample)
-		if e:
-			accum.accumulate(e)
-	for v in accum.found():
-		m,b = v
-		if m != float('inf'):
-			cv2.line(original, (int(0), int(b)), (int(800), int(800*m+b)), (0, 255, 0))
-		else:
-			cv2.line(original, (int(b), 0), (int(b), int(600)), (0, 255, 0))
-
 class Accumulator:
 	
 	def __init__(self):
-		self.accum = {}
 		self.thrd = 2
+		self.epouchs = 10
+		self.iters = 100
+		self.sample_size = 2
 
 	def key_for(self, params):
 		return None
+
+	def fit_curve(self, img, sample):
+		return None
+
+	def on_curve(self, curve):
+		return False
 
 	def accumulate(self, params):
 		key = self.key_for(params)
@@ -189,6 +166,47 @@ class Accumulator:
 			if v[1] > self.thrd:
 				yield v[0]
 
+	def best_curve(self):
+		maxW = 0
+		curve = None
+		for v in self.accum.values():
+			if v[1] > maxW:
+				maxW = v[1]
+				curve = v[0]
+		if maxW > self.thrd:
+			return curve
+		else:
+			return None
+
+	# generator over all detected curves
+	def __call__(self, img):
+		# Get points
+		self.accum = {}
+		self.points = []
+		it = np.nditer(img, flags=["multi_index"])
+		while not it.finished:
+			if it[0] > 0:
+				self.points.append((it.multi_index[1], it.multi_index[0]))
+			it.iternext()
+		# iterate across epouchs
+		for i in xrange(self.epouchs):
+			accum = {}
+			for j in xrange(self.iters):
+				sample = random.sample(self.points, self.sample_size)
+				curve = self.fit_curve(img, sample)
+				if curve: # None if failed to fit
+					self.accumulate(curve)
+			curve = self.best_curve()
+			if curve != None:
+				new_points = filter(lambda p: not self.on_curve(curve, p), self.points)
+				# test for sufficient amount of removed points
+				if len(self.points) - len(new_points) > self.min_curve:
+					print curve, len(self.points) - len(new_points)
+					yield curve
+					self.points = new_points
+		raise StopIteration
+
+
 class EllipseAccum(Accumulator):
 
 	def __init__(self, tolerance):
@@ -199,21 +217,56 @@ class EllipseAccum(Accumulator):
 	def key_for(self, params):
 		return int(params[0])/self.tol, int(params[1])/self.tol, int(params[2])/self.tol,int(params[3])/self.tol
 
+	def fit_curve(self, img, sample):
+		pass
+
+
 class LinesAccum(Accumulator):
 
-	def __init__(self, tolerance):
-		self.accum = {}
-		self.thrd = 10
+	def __init__(self, epouchs=10, iters=100, tolerance=1, threshold=2, min_curve=12):
+		self.thrd = threshold
+		self.sample_size = 2
+		self.min_curve = min_curve
 		self.tol = tolerance
+		self.epouchs = epouchs
+		self.iters = iters
+		self.on_curve_tol = tolerance
 
 	def key_for(self, params):
 		if params[0] == float('inf'):
-			return int(params[1])/self.tol
+			return params[1]
 		else:
-			return int(params[0])/self.tol, int(params[1])/self.tol
+			return params[0], int(params[1])
 
-#rht_ellipses(img, 2000)
-rht_lines(img, 2000)
+	def fit_curve(self, img, sample):
+		x1,y1 = sample[0]
+		x2,y2 = sample[1]
+		if x1 != x2: 
+			m = (y1 - y2)/(x1 - x2)
+			b = y1 - x1 * m
+		else:
+			m = float('inf')
+			b = x1
+		return m,b
+
+	def on_curve(self, curve, point):
+		x,y = point
+		m,b = curve
+		if m == float('inf') and abs(b - x) < self.on_curve_tol:
+			return True
+		elif abs(x*m+b - y) < self.on_curve_tol:
+			return True
+		return False
+
+lines = LinesAccum(epouchs=50, iters=100, tolerance=2)
+for (m,b) in lines(img):
+	if m != float('inf'):
+		cv2.line(original, (int(0), int(b)), (int(800), int(800*m+b)), (0, 255, 0))
+	else:
+		cv2.line(original, (int(b), 0), (int(b), int(600)), (0, 255, 0))
+
+# rht_ellipses(img, 2000)
+
 #cv2.imshow('After canny + filter', img)
 #cv2.waitKey(0)
 cv2.imshow('Some image', original)
